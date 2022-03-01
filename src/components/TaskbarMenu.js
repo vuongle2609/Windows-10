e755088e4f9a10dcb4ff387306ac0fa4782ce83a
Nguyen Huy Hoang <19t6n2003@gmail.com>
2022-03-02 03:13:42 +0700

    suffering on nights

--- src/components/TaskbarMenu.js
+++ src/components/TaskbarMenu.js
@@ -1,4 +1,5 @@
 import useStore from "../store";
+import { useState } from "react";
 
 const TaskbarMenuItem = ({ icon, func }) => {
   return (
@@ -11,8 +12,49 @@ const TaskbarMenuItem = ({ icon, func }) => {
   );
 };
 
+const SideBarMenu = () => {
+  const { setSettings } = useStore();
+  const [sideBar, setSideBar] = useState(false)
+
+  const handleSideBar = setTimeout(() => {
+    console.log("moing")
+    setSideBar(true)
+  }, 1000);
+
+  const handleIn = () => {
+    handleSideBar()
+  }
+
+  const handleOut = () => {
+    console.log("donging")
+    setSideBar(false)
+    clearTimeout(handleSideBar)
+  }
+
+  return (
+    <div
+      onMouseEnter={() => {
+        if (!sideBar) handleIn()
+      }}
+      onMouseLeave={handleOut}
+      className={"h-full bg-[#e4e4e4] flex flex-col justify-between absolute left-0 top-0" + (!sideBar ? " w-[48px]" : " w-[100px]")}>
+      <div>
+        <TaskbarMenuItem icon="bars" />
+      </div>
+      <div className="flex flex-col w-full items-center text-[20px]">
+        <TaskbarMenuItem icon="user" />
+        <TaskbarMenuItem icon="file" />
+        <TaskbarMenuItem icon="image" />
+        <TaskbarMenuItem icon="gear" func={() => setSettings(true)} />
+        <TaskbarMenuItem icon="power-off" />
+      </div>
+    </div>
+  );
+};
+
 const TaskbarMenu = () => {
-  const { menuOpen, setSettings } = useStore();
+  const { menuOpen } = useStore();
+
   return (
     <div
       className={
@@ -20,19 +62,12 @@ const TaskbarMenu = () => {
         (menuOpen ? " bottom-[40px]" : " -bottom-full")
       }
     >
-      <div className="w-[48px] h-full bg-[#e4e4e4] flex flex-col justify-between">
-        <div>
-          <TaskbarMenuItem icon="bars" />
-        </div>
-        <div className="flex flex-col w-full items-center text-[20px]">
-          <TaskbarMenuItem icon="user" />
-          <TaskbarMenuItem icon="file" />
-          <TaskbarMenuItem icon="image" />
-          <TaskbarMenuItem icon="gear" func={() => setSettings(true)} />
-          <TaskbarMenuItem icon="power-off" />
+      <div className="w-full h-full bg-red-200 relative">
+        <SideBarMenu />
+        <div className="w-[274px] h-full bg-[#e4e4e4] ml-[48px]">
+          fbnamnfbanmsfnmbsanb
         </div>
       </div>
-      <div className="w-[274px] h-full bg-[#e4e4e4]"></div>
     </div>
   );
 };
